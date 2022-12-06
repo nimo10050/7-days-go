baecbda916335fc5b7a3c38c944023987c9d0465
yangyuru <yiming2008@126.com>
2022-12-07 00:01:34 +0800

    GeeCache Study day02.

---
+++ geecache.go
@@ -0,0 +1,66 @@
+package geecache
+
+import (
+	"sync"
+)
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.Lock()
+	defer mu.Unlock()
+	g := groups[name]
+	return g
+}
+
+func (g *Group) load(key string) (v ByteView, err error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.mainCache.add(key, value)
+	return value, nil
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if v, ok := g.mainCache.get(key); ok {
+		return v, nil
+	}
+	return g.load(key)
+}
