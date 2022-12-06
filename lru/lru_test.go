525f70ed7cdb093d0127852b0b116c59d9e9fcb9
zhanggaopei <nimo10050@gmail.com>
2022-12-06 21:55:00 +0800

    add test_lru unit test

---
+++ lru/lru_test.go
@@ -0,0 +1,25 @@
+package lru
+
+import (
+	"fmt"
+	"testing"
+)
+
+type String string
+
+func (s String) Len() int64 {
+	return int64(len(s))
+}
+
+func TestGet(t *testing.T) {
+	lru := New(int64(90), nil)
+
+	key := "zhangsan"
+
+	lru.Add(key, String("123"))
+
+	if value, ok := lru.Get(key); ok {
+		fmt.Println("cache value: ", value)
+	}
+
+}
